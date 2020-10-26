81d241fd31ade8eb78dec3d66156cbcf94f87dba
William Santos <williamdsw@outlook.com>
2020-10-25 21:29:58 -0300

    Code refactoring for TableChart component

--- src/app/google-chart/table-chart/table-chart.component.spec.ts
+++ src/app/google-chart/table-chart/table-chart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TableChartComponent } from './table-chart.component';
 
@@ -6,7 +6,7 @@ describe('TableChartComponent', () => {
   let component: TableChartComponent;
   let fixture: ComponentFixture<TableChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TableChartComponent ]
     })
--- src/app/google-chart/table-chart/table-chart.component.ts
+++ src/app/google-chart/table-chart/table-chart.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { GoogleChartService } from '../service/google-chart.service';
 
 @Component({
   selector: 'app-table-chart',
-  templateUrl: './table-chart.component.html',
-  styleUrls: ['./table-chart.component.css']
+  templateUrl: './table-chart.component.html'
 })
 export class TableChartComponent implements OnInit {
+
+  @ViewChild('tableChartContainer', { static: true })
+  private tableChartContainer: ElementRef;
 
   private chartLibrary: any;
 
@@ -17,12 +19,10 @@ export class TableChartComponent implements OnInit {
     this.chartLibrary.charts.setOnLoadCallback (this.drawChart.bind (this));
    }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   private drawChart() {
-    const divTableChart = document.getElementById ('divTableChart');
-    const chart = new this.chartLibrary.visualization.Table (divTableChart);
+    const chart = new this.chartLibrary.visualization.Table (this.tableChartContainer.nativeElement);
     const data = new this.chartLibrary.visualization.DataTable();
     data.addColumn ('string', 'Year');
     data.addColumn ('number', 'Sales');
@@ -32,6 +32,10 @@ export class TableChartComponent implements OnInit {
       ['2005', 1170, 460],
       ['2006', 660, 1120],
       ['2007', 1030, 540],
+      ['2008', 2000, 1600],
+      ['2009', 1845, 3000],
+      ['2010', 452.56, 100.12],
+      ['2011', 3123.23, 2000.41]
     ]);
 
     const options = { title: 'Sales Tubular Data', width: '100%', height: '100%' };

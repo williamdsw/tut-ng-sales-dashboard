32957386f31836d73220eef57febf773d16db6c5
William Santos <williamdsw@outlook.com>
2020-11-23 13:31:54 -0300

    Generating deployables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
 import { GoogleChartModule } from './google-chart/google-chart.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
